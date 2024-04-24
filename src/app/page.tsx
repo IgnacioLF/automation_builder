266edc04e449ffe4f27ff752a3c1fe67261bd033
IgnacioLF <ignaciolopezfeliu@gmail.com>
2024-04-24 21:24:00 +0200

    -added infiniteCards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { ContainerScroll } from "@/components/global/Container-scroll-animation";
+import { InfiniteMovingCards } from "@/components/global/Infinite-moving-cards";
 import Navbar from "@/components/global/Navbar";
 import { Button } from "@/components/ui/button";
+import { clients } from "@/lib/constant";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex items-center justify-center flex-col">
       <Navbar />
       <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
-        <div className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="h-screen w-full bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
           {/*Gradient backgorund*/}
-          <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+          <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
           <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
             <ContainerScroll
               titleComponent={
@@ -31,6 +33,13 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
     </main>
   );
 }
