a4969f8cb68deb5644fced048dd08292adf7ae36
IgnacioLF <ignaciolopezfeliu@gmail.com>
2024-05-02 15:50:20 +0200

    - added profile picture storage functionality

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,10 +1,25 @@
 import ProfileForm from "@/components/forms/profile-form";
+import ProfilePicture from "./_components/profile-picture";
 import React from "react";
+import { db } from "@/lib/db";
 
 type Props = {};
 
 const Settings = (props: Props) => {
-  //TODO Profile picture
+  //TODO auth w clerk
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: "",
+      },
+    });
+
+    return response;
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -18,6 +33,11 @@ const Settings = (props: Props) => {
             Add or update your information
           </p>
         </div>
+        <ProfilePicture
+          onDelete={removeProfileImage}
+          userImage={user?.profileImage || ""}
+          onUpload={uploadProfileImage}
+        ></ProfilePicture>
         <ProfileForm />
       </div>
     </div>
